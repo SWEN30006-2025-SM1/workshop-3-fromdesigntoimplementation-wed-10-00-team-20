24b41a395fea0d4e6f6cf8a36d3693efa7ae7b9a
xianm2 <xian.ma@student.unimelb.edu.au>
2025-03-26 11:32:28 +1100

    update Person.java,Staff.java,Student.java

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -0,0 +1,12 @@
+package com.unimelb.swen30006.workshops;
+
+public class Student extends Person{
+    private String email;
+    private String studentID;
+
+
+    public void sendEmailMsg(){}
+
+    public float totalGrade() {}
+
+}
